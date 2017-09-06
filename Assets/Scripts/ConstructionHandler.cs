03abfd6b3f8ddc5beb32028bdbadfa713fc4f61a
itai.yavin <itai.yavin@gmail.com>
2017-09-06 11:39:41 +0200

    ConstructionHandler
    Created a constructionhandler which holds all of the different kind of constructables, its also responsible of distributing the values upon picking up a number as Hugo.
    
    Made  Prefab for the ConstructionHandler

--- Assets/Scripts/ConstructionHandler.cs
+++ Assets/Scripts/ConstructionHandler.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConstructionHandler : MonoBehaviour {
+	public Constructable[] ones, twos, threes, fours, fives;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void SendResource(int value){
+		int rnd;
+		switch(value){
+			case 1:
+				rnd = Random.Range(0, ones.Length);
+				ones[rnd].Construct();
+			break;
+
+			case 2:
+				rnd = Random.Range(0, twos.Length);
+				twos[rnd].Construct();
+			break;
+			
+			case 3:
+				rnd = Random.Range(0, threes.Length);
+				threes[rnd].Construct();
+			break;
+			
+			case 4:
+				rnd = Random.Range(0, fours.Length);
+				fours[rnd].Construct();
+			break;
+			
+			case 5:
+				rnd = Random.Range(0, fives.Length);
+				fives[rnd].Construct();
+			break;
+			
+			default:
+				Debug.Log("THIS VALUE IS NOT A VALID CONSTRUCTION VALUE");
+			break;
+		}
 	}
 }
