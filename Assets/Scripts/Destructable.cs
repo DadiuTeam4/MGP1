5bebd3858fb3986bdc0ac1da925301563f4c0fad
itai.yavin <itai.yavin@gmail.com>
2017-09-06 11:03:07 +0200

    Minor Change for Destructable
    Destructables are now disabled upon destruction.

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -20,7 +20,7 @@ public class Destructable : Touchable {
 
 	override public void Interact(RaycastHit hit){
 		if(Vector3.Distance(hugo.position, transform.position) < activateDistance){
-			int amount = Random.Range(3, 100);
+			int amount = Random.Range(3, 10);
 			int rnd;
 			
 			Vector3 rndVec;
@@ -31,11 +31,13 @@ public class Destructable : Touchable {
 				rnd = Random.Range(0, numbers.Length);
 				g = Instantiate(numbers[rnd], hit.point, Quaternion.identity).GetComponent<Number>();
 
-				rndVec = new Vector3(Random.Range(-1,1),Random.Range(-1,1),Random.Range(-1,1));
+				rndVec = new Vector3(Random.Range(-1,1),Random.Range(0,1),Random.Range(-1,1));
 				g.SetBurstDiretion(rndVec.normalized);
 				g.SetBurstSpeed(Random.Range(80, 280));
 				g.Burst();
 			}
+
+			transform.gameObject.SetActive(false);
 		}
 	}
 }
