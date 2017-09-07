07f5440807e0b266557d483dc96df639917f4f9f
itai.yavin <itai.yavin@gmail.com>
2017-09-07 09:56:26 +0200

    Refactered Constructable
    Constructable is now an interface making it easier to extend and add functionality.
    
    Added a ScaleConstructable script and a ColourConstructable script which uses the construct interface.

--- Assets/Scripts/ConstructionHandler.cs
+++ Assets/Scripts/ConstructionHandler.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConstructionHandler : MonoBehaviour {
-	public Constructable[] ones, twos, threes, fours, fives;
+	public GameObject[] ones, twos, threes, fours, fives;
 
 	public void SendResource(int value){
 		int rnd;
 		switch(value){
 			case 1:
 				rnd = Random.Range(0, ones.Length);
-				ones[rnd].Construct();
+				ones[rnd].GetComponent<Constructable>().Construct();
 			break;
 
 			case 2:
 				rnd = Random.Range(0, twos.Length);
-				twos[rnd].Construct();
+				twos[rnd].GetComponent<Constructable>().Construct();
 			break;
 			
 			case 3:
 				rnd = Random.Range(0, threes.Length);
-				threes[rnd].Construct();
+				threes[rnd].GetComponent<Constructable>().Construct();
 			break;
 			
 			case 4:
 				rnd = Random.Range(0, fours.Length);
-				fours[rnd].Construct();
+				fours[rnd].GetComponent<Constructable>().Construct();
 			break;
 			
 			case 5:
 				rnd = Random.Range(0, fives.Length);
-				fives[rnd].Construct();
+				fives[rnd].GetComponent<Constructable>().Construct();
 			break;
 			
 			default:
