f4a88f08ca067161a69c0cf593adacdf6a58dd64
itai.yavin <itai.yavin@gmail.com>
2017-09-07 10:49:07 +0200

    WIP working on timed event

---
+++ Assets/Scripts/MoveToConstructable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToConstructable : MonoBehaviour {
+	public Transform to;
+	public float speed;
+
+	void Update () {
+        float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, to.position, step);		
+	}
+}
