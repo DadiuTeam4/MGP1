0d1d11d0e1bbb0b11c7dab74656ecf1ccb35b694
itai.yavin <itai.yavin@gmail.com>
2017-09-07 13:03:24 +0200

    Changed the number generation
    Now you will be sure to get at least one of each kind of number. The rest will be random. Should you have a lesser amount of numbers spawned, than there are of number kinds, you will of course not get one of each.

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -30,25 +30,32 @@ public class Destructable : MonoBehaviour, Holdable {
 			if(Vector3.Distance(hugo.position, transform.position) < activateDistance){
 				int amount = Random.Range(numberMinAmount, numberMaxAmount);
 				int rnd;
-				
-				Vector3 rndVec;
 
-				Number g;
 
 				for(int i = 0; i < amount; i++){
-					rnd = Random.Range(0, numbers.Length);
-					g = Instantiate(numbers[rnd], hit.point, Quaternion.identity).GetComponent<Number>();
-
-					g.SetConstructionHandler(constructionHandler);
-
-					rndVec = new Vector3(Random.Range(-1,1),Random.Range(0,1),Random.Range(-1,1));
-					g.SetBurstDirection(rndVec.normalized);
-					g.SetBurstSpeed(Random.Range(minimumSpeed, maximumSpeed));
-					g.Burst();
+					if(i >= numbers.Length){
+						rnd = Random.Range(0, numbers.Length);
+						InstantiateNumber(rnd, hit.point);
+					} else {
+						InstantiateNumber(i, hit.point);
+					}
 				}
 
 				transform.gameObject.SetActive(false);
 			}
 		}
+	}
+
+	public void InstantiateNumber(int numberID, Vector3 position){
+		Number g;
+		Vector3 rndVec;
+		g = Instantiate(numbers[numberID], position, Quaternion.identity).GetComponent<Number>();
+
+		g.SetConstructionHandler(constructionHandler);
+
+		rndVec = new Vector3(Random.Range(-1,1),Random.Range(0,1),Random.Range(-1,1));
+		g.SetBurstDirection(rndVec.normalized);
+		g.SetBurstSpeed(Random.Range(minimumSpeed, maximumSpeed));
+		g.Burst();
 	}
 }
