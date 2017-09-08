f755ab20a564450d424ca601908229970372d79d
Unknown <tilemachospantazis8@gmail.com>
2017-09-08 16:03:28 +0200

    Comments and fix on track change

--- Assets/RhytmScript.cs
+++ Assets/RhytmScript.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Manages the behaviour of all rythm objects
+//when you click on something on time it vanishes but it is not destroyed
+
 public class RhytmScript : MonoBehaviour, Touchable {
-	public Timing3 t3; 
-	private bool activate = false;
-	public float interval = 2.0f;
-	private MeshRenderer mR; 
-	private Collider collider;
-	private bool destroyed = false; 
-	public int lvl = 0; 
-	public int maxLvl; 
-	private string[] levels = { "State1", "State2", "State3", "State4" };
-    //this has t
+	public Timing3 t3; //reference to the timer
+	private bool activate = false;// am I clickable or not
+	public float interval = 2.0f;// Where my beat is
+	private MeshRenderer mR; //in order to turn off the mesh
+	private Collider collider;//in order to turn off the collider
+	private bool destroyed = false; // am I destroyed
+	public int lvl = 0; //need fixing, needs to be on timing3
+	private string[] levels = { "State1", "State2", "State3", "State4" };//what the songs are
+
 	// Use this for initialization
 	void Start () {
 		collider = gameObject.GetComponent<Collider>();
@@ -23,44 +25,42 @@ public class RhytmScript : MonoBehaviour, Touchable {
 	// Update is called once per frame
 	void Update () {
 
+		//if I am on beat
 		if (0.0f < (t3.currentTime) - (interval) && (t3.currentTime) - (interval) < 0.11f) {
-			interval += t3.GetTargetTime();
-			activate = true;
-			gameObject.GetComponent<Renderer>().material.color = Color.green;
+			interval += t3.GetTargetTime();//move my interval to the next loop 
+			activate = true;//Activate me
+			gameObject.GetComponent<Renderer>().material.color = Color.green;//make it visible that I am active, turn me green
 
 		} 
 
-		if(t3.currentTime - interval + t3.GetTargetTime() > 0.3f )
+		if(t3.currentTime - interval + t3.GetTargetTime() > 0.3f )//if more than 0.3 sec has passed
 		{
-			if (!destroyed && activate) {
-				t3.SetFail (true);
-
+			if (!destroyed && activate) {//and I am not destroyed but I was active
+				t3.SetFail (true);//I failed
 			}
-
-		}
-			
+		}		
 	}
 
+//Manages interaction with the objects 
 	public void Interact(RaycastHit hit){
-		if(activate){
+		if(activate){//if I am active and got hit
+			//turn everything off and declare that I am now destroyed, also change song
 			mR.enabled = false;
 			collider.enabled = false;
 			destroyed = true;
-			lvl++; 
-
-			AkSoundEngine.SetState ("Mechanics_Rhytm", levels [lvl]);
+			t3.ChangeSong();
 		}
 	}
 
+//Resets state 
 	public void Reset(){
-
+		//turn everything back to the initial state
 		destroyed = false;
 		mR.enabled = true;
 		collider.enabled = true;
 		activate = false;
 		gameObject.GetComponent<Renderer>().material.color = Color.white;
-		lvl = 0;
-		AkSoundEngine.SetState ("Mechanics_Rhytm", levels [lvl]);
+
 	}
 
 
--- Assets/Timing3.cs
+++ Assets/Timing3.cs
@@ -2,52 +2,43 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+//this script keeps track of time and what the status of all the Rythm object is
 public class Timing3: MonoBehaviour {
 
-	private float targetTime = 8f;
-	public float currentTime = 0.0f;
-	private bool fail = false;
-	public RhytmScript[] rhythmicObjects;
-
-	void Awake(){
-
-	}
-
+	private int whichSongToPlay = 0;//which song to play indexing
+	private float targetTime = 8f;//end of a beat-loop
+	private string[] levels = { "State1", "State2", "State3", "State4" };//what the songs are
+	public float currentTime = 0.0f;//how much time has passed since last time
+	public RhytmScript[] rhythmicObjects;//array of all the objects that follow the beat
 
 	void Update(){
-
+		//keep track of time
 		currentTime += Time.deltaTime;
-
-		if (currentTime >= 8f)
-		{
-		//	timerEnded();
-		}
-		//Debug.Log (currentTime); 
-
 	}
 
+	//Getter for how much time the loop takes
 	public float GetTargetTime(){
 		return targetTime;
 	}
+	//Someone has failed. Alert all rythmic objects to reset
 	public void SetFail(bool someoneFail){
-		fail = someoneFail;
-		foreach (RhytmScript obj in rhythmicObjects) {
-			obj.Reset ();
 		
+		AkSoundEngine.SetState ("Mechanics_Rhytm", levels [0]);
+		whichSongToPlay = 0;
+		foreach (RhytmScript obj in rhythmicObjects) {//for all the rythmic objects
+			obj.Reset ();	//reset
 		}
 
 	}
-	public bool GetFail()
-	{
-		return fail;
-	}
 
-	void timerEnded()
-	{
-		//do your stuff here.
-		print ("8 secs have passed"); 
-		//currentTime = 0.0f;
-	}
+//Change the song
+	public void ChangeSong(){
+		
+		whichSongToPlay++;//move the index
+	
+		AkSoundEngine.SetState ("Mechanics_Rhytm", levels [whichSongToPlay]);//which song to play
 
+	}
 
 }
