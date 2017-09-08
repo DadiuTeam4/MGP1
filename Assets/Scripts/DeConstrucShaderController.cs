80e977335d5fa742631d1ecabb3d8f7a449081dc
SlyHoneyBadger <pjager13@student.aau.dk>
2017-09-08 10:24:06 +0200

    DeConstruct Shader

---
+++ Assets/Scripts/DeConstrucShaderController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeConstrucShaderController : MonoBehaviour {
+
+    Material material;
+    Renderer rend;
+    float num = 0;
+    bool active;
+
+    [SerializeField]
+    GameObject GO;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        material = rend.material;
+        //GO.GetComponent<>
+	}
+	
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeTex(1);
+            active = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ChangeTex(0);
+            num = 0;
+            DeconstructAnim(num);
+            active = false;
+        }
+
+        else if (active)
+        {
+            num += 2f;
+            DeconstructAnim(num);
+        }
+        num = Mathf.Clamp(num, 0, 1000);
+
+    }
+
+    public void ChangeTex(float _value)
+    {
+        material.SetFloat("_Gradiant", _value);
+    }
+
+
+	// Update is called once per frame
+    public void DeconstructAnim(float _value)
+    {
+        material.SetFloat("_Intensity", _value);
+    }
+
+    
+
+}
