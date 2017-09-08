9b76e6558df124254e3faa3639814499cde524e1
DSHOII <jonathan.shh@gmail.com>
2017-09-08 11:33:52 +0200

    changes to build script

--- Assets/Editor/buildScript.cs
+++ Assets/Editor/buildScript.cs
@@ -1,12 +1,22 @@
-﻿// C# example
+﻿using UnityEngine;
+using System.Collections;
 using UnityEditor;
-class MyEditorScript
+using System;
+using System.Collections.Generic;
+ 
+public class BuildScript : MonoBehaviour {
+ 
+static void PerformBuild ()
 {
-     static void PerformBuild ()
-     {
-         string[] scenes = { "Assets/Scenes/ProtoScene1.unity" };
-         BuildPipeline.BuildPlayer(scenes, ...);
-     }
+
+BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+buildPlayerOptions.scenes = new[] {"Assets/Scenes/ProtoScene1.unity", "Assets/Scenes/ProtoScene2.unity"};
+buildPlayerOptions.locationPathName = "Android";
+buildPlayerOptions.target = BuildTarget.Android;
+buildPlayerOptions.APP_NAME = "Drengen der talte alting";
+buildPlayerOptions.TARGET_DIR = "Builds/";
+buildPlayerOptions.options = BuildOptions.None;
+
+BuildPipeline.BuildPlayer(buildPlayerOptions);
+
 }
-
-
