0d78636355850b25e6e245c0202dd1dcf14ad927
You Wu <3085031@sina.com>
2017-09-08 17:05:20 +0800

    Separate the blink text and the open scence controller
    Separate the blink text and the open scence controller

--- Assets/Scripts/OpenSceneController.cs
+++ Assets/Scripts/OpenSceneController.cs
@@ -7,33 +7,15 @@ using UnityEngine.UI;
 public class OpenSceneController : MonoBehaviour
 {
 
-    Text text;
-    private bool isBlink = true;
     // Use this for initialization
-    void Start()
-    {
-        text = GetComponent<Text>();
-        StartCoroutine(blink());
-    }
+    public string nextScence = "Deconstructable";
 
     // Update is called once per frame
     void Update()
     {
         if (Input.touchCount == 1)
         {
-            SceneManager.LoadScene("Deconstructable", LoadSceneMode.Single);
-        }
-
-    }
-
-    IEnumerator blink()
-    {
-        while (isBlink)
-        {
-            text.text = "";
-            yield return new WaitForSeconds(0.5f);
-            text.text = "Touch to Start ";
-            yield return new WaitForSeconds(0.5f);
+            SceneManager.LoadScene(nextScence, LoadSceneMode.Single);
         }
 
     }
