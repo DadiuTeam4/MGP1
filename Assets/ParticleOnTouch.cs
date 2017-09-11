affad794779091f87cbb8a9e961d4109fe36067a
SlyHoneyBadger <pjager13@student.aau.dk>
2017-09-11 11:25:20 +0200

    Hugo Animation Sprict Controller

---
+++ Assets/ParticleOnTouch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOnTouch : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem particle;
+
+    TouchHandler touchHandler;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //if()
+
+	}
+}
---
+++ Assets/Scripts/HugoInteractsWithOject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HugoInteractsWithOject : MonoBehaviour {
+
+    Destructable GO;
+    public HugoAnimation hugoAnim;
+
+    // Use this for initialization
+    void Start () {
+       GO = GetComponent<Destructable>();
+       //hugoAnim = hugoAnim.GetComponent<HugoAnimation>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GO.GetHasBeenSetToDestroy())
+        {
+            hugoAnim.SetCountFingers(true);
+        }
+	}
+}
