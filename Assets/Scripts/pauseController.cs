095af6872aeb9e2a7d5b29d0a3c5b767c5baeaee
You Wu <3085031@sina.com>
2017-09-12 16:29:14 +0800

    Added the language controller and add a drop down menu for the language."
    Added the language controller and add a drop down menu for the language."

--- Assets/Scripts/pauseController.cs
+++ Assets/Scripts/pauseController.cs
@@ -9,8 +9,6 @@ public class pauseController : MonoBehaviour
     public Button pauseBtn;
     public Text text;
     private bool isPaused = false;
-
-    public GameObject languageOption;
     // Use this for initializati<on
     void Start()
     {
@@ -21,23 +19,15 @@ public class pauseController : MonoBehaviour
     {
         if (!isPaused)
         {
-            if (languageOption != null)
-            {
-                languageOption.SetActive(true);
-            }
             Time.timeScale = 0;
             text.GetComponent<Text>().text = "Resume";
-            isPaused = true;
+			isPaused = true;
         }
         else
         {
-            if (languageOption != null)
-            {
-                languageOption.SetActive(false);
-            }
             Time.timeScale = 1;
             text.GetComponent<Text>().text = "Pause";
-            isPaused = false;
+			isPaused = false;
         }
     }
 
