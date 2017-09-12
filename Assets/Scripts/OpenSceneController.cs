6353f7e5716859349d360b79909acb25748eac2d
SlyHoneyBadger <pjager13@student.aau.dk>
2017-09-12 11:36:42 +0200

    Opening Scene setup

--- Assets/Scripts/OpenSceneController.cs
+++ Assets/Scripts/OpenSceneController.cs
@@ -8,18 +8,54 @@ public class OpenSceneController : MonoBehaviour
 {
 
     // Use this for initialization
+
+
+
     public string nextScene;
+    public bool danish;
+    public Transform danishTransform;
+    public bool english;
+    public Transform englishTransform;
+    public Transform StartBTN;
+    public Transform Hugo; 
 
 
     // Update is called once per frame
     void Update()
     {
-		//Touchcount for tablet, anykeydown for mouse/unity test interface
+	/*	//Touchcount for tablet, anykeydown for mouse/unity test interface
 		if (Input.touchCount > 0 ||Input.anyKeyDown)
         {
 			SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
 
         }
+        */
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+    }
+
+    public void Danish()
+    {   
+        danish = true;
+        danishTransform.gameObject.SetActive(false);
+        englishTransform.gameObject.SetActive(false);
+        StartBTN.gameObject.SetActive(true);
+
+    }
+
+    public void English()
+    {
+        english = true;
+        danishTransform.gameObject.SetActive(false);
+        englishTransform.gameObject.SetActive(false);
+        StartBTN.gameObject.SetActive(true);
+    }
+
+    void MoveHugo()
+    {
 
     }
 }
