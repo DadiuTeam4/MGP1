f6edf58cd3b6546262fa7e1b9b454c5c1998ec06
Emil Imbert Villumsen <emimvi@gmail.com>
2017-09-12 14:39:01 +0200

    Fix menu popup

--- Assets/Scripts/pauseController.cs
+++ Assets/Scripts/pauseController.cs
@@ -6,30 +6,40 @@ using UnityEngine.UI;
 public class pauseController : MonoBehaviour
 {
 
-    public Button pauseBtn;
-    public Text text;
-    private bool isPaused = false;
-    // Use this for initializati<on
-    void Start()
-    {
-        pauseBtn.GetComponent<Button>().onClick.AddListener(tasksOnClick);
-    }
+	public Button pauseBtn;
+	public Text text;
+	private bool isPaused = false;
 
-    void tasksOnClick()
-    {
-        if (!isPaused)
-        {
-            Time.timeScale = 0;
-            text.GetComponent<Text>().text = "Resume";
+	public GameObject languageOption;
+	// Use this for initializati<on
+	void Start()
+	{
+		pauseBtn.GetComponent<Button>().onClick.AddListener(tasksOnClick);
+	}
+
+	void tasksOnClick()
+	{
+		if (!isPaused)
+		{
+			if (languageOption != null)
+			{
+				languageOption.SetActive(true);
+			}
+			Time.timeScale = 0;
+			text.GetComponent<Text>().text = "Resume";
 			isPaused = true;
-        }
-        else
-        {
-            Time.timeScale = 1;
-            text.GetComponent<Text>().text = "Pause";
+		}
+		else
+		{
+			if (languageOption != null)
+			{
+				languageOption.SetActive(false);
+			}
+			Time.timeScale = 1;
+			text.GetComponent<Text>().text = "Pause";
 			isPaused = false;
-        }
-    }
+		}
+	}
 
 
 }
