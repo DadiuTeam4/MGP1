1e96fc40a61e88a9ba9487502dabf98d552a1c14
Patrick Vincent Højstrøm <patrick.vincent@live.dk>
2017-09-12 15:02:26 +0200

    Auto stash before merge of "dev" and "origin/dev"

---
+++ Assets/Scripts/Constructor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Constructor : MonoBehaviour, Constructable
+{
+    public Transform[] elements;
+    public float animationTime;
+
+    private int elementIndex = 0;
+    private int amountOfNumbers;
+    void Start()
+    {
+        amountOfNumbers = elements.Length;
+        foreach (Transform element in elements)
+        {
+            element.localScale = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void Construct()
+    {
+        if (elementIndex == amountOfNumbers)
+        {
+            return;
+        }
+        StartCoroutine(Grow(elements[elementIndex]));
+        elementIndex++;
+    }
+
+    IEnumerator Grow(Transform element)
+    {
+        float currentTime = 0;
+        while (currentTime < animationTime)
+        {
+            currentTime += Time.deltaTime;
+            print((currentTime / animationTime));
+            element.localScale = Vector3.Lerp(new Vector3(), new Vector3(1, 1, 1), (currentTime / animationTime));
+            yield return null;
+        }
+    }
+}
