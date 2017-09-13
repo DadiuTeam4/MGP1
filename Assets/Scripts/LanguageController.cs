9a820bab0ef8d86412e5d41c0d80f32e2eac9542
You Wu <rmq350@ku.dk>
2017-09-13 11:55:08 +0200

    Added script of xml language change.
    Added script of xml language change.

--- Assets/Scripts/LanguageController.cs
+++ Assets/Scripts/LanguageController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Xml;
 
 public class LanguageController : MonoBehaviour
 {
 
     // Use this for initialization
+    public Hashtable allStrings;
     void Start()
     {
         Dropdown dropdown = gameObject.GetComponent<Dropdown>();
@@ -33,7 +35,23 @@ public class LanguageController : MonoBehaviour
 
     private void setDanish()
     {
-
+        XmlDocument xml = new XmlDocument();
+        xml.Load("Assets/lang.xml");
+        allStrings = new Hashtable();
+        var element = xml.GetElementById("dan");
+        if (element != null) 
+        { 
+           IEnumerator ele =  element.GetEnumerator();
+           while(ele.MoveNext())
+           {
+               string name = (ele.Current as XmlElement).GetAttribute("name");
+               string value = (ele.Current as XmlElement).InnerText;
+               allStrings.Add(name, value);
+               Debug.Log(name + ", " + value);
+           }
+        } else {
+            Debug.LogError("The specified language does not exist: " + element);
+        }
     }
 
     private void setEnglish()
