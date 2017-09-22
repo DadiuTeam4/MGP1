fc146cfbcc3729b025b006ba35b57806561e9ccf
You Wu <rmq350@ku.dk>
2017-09-22 14:01:36 +0200

    Added build Script
    Added build Script

---
+++ Assets/Editor/buildScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+using System.Collections;
+
+using UnityEditor;
+
+using System;
+
+using System.Collections.Generic;
+
+
+
+public class BuildScript : MonoBehaviour
+
+{
+
+    [MenuItem("MyTools/Jenkins build test")]
+
+    public static void PerformBuild()
+
+    {
+
+        Debug.Log("run the menu item");
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/OpenScene.unity", "Assets/Scenes/ProtoScene1.unity", 
+                                            "Assets/Scenes/ProtoScene3.unity", "Assets/Scenes/ProtoScene4.unity" };
+
+        buildPlayerOptions.locationPathName = "Builds/  .apk";
+
+        buildPlayerOptions.target = BuildTarget.Android;
+
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+    }
+
+}
